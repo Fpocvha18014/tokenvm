6e3c45b27897659aae12b740460e58273a6550ca
Fpocvha18014 <nkemmoses54@gmail.com>
2024-07-14 09:57:41 +0000

    mattcoin

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Macain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Matt"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Mattcoin"
 )
 
 var ID ids.ID
